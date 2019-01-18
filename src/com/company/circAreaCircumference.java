af53e5efcb4e5f6e31deed7ca3cee49eb39281f3
tedasare <46811145+tedasare@users.noreply.github.com>
2019-01-18 23:56:27 +0000

    Update circAreaCircumference.java

--- src/com/company/circAreaCircumference.java
+++ src/com/company/circAreaCircumference.java
@@ -14,8 +14,8 @@ public class circAreaCircumference {            //method for outputting the area
         Scanner reader = new Scanner(System.in);    //method for accepting input
         System.out.println("Enter a number: ");
         int radius = reader.nextInt();              //assigning an integer to the input
-        Double area = Math.PI * (radius * radius);  //calculating the area
-        Double circ = 2 * Math.PI * radius;         //calculating the circumference
+        double area = Math.PI * (radius * radius);  //calculating the area
+        double circ = 2 * Math.PI * radius;         //calculating the circumference
         System.out.printf("The area of the circle  given the radius: " + radius + " is %.2f", area);
         System.out.printf(" and the circumference is: %.2f\n", circ);
         reader.close();
